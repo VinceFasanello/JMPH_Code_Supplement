3869cabec76cdc2d2821b15f3fa938569e22acdc
Vincent Fasanello <vincefasanello@gmail.com>
2020-06-22 10:51:09 -0500

    Cluster Files Added to Dir
    Cluster outputs for e, t, p added to directory

--- ClusterFiles_T_UNUSED/createBatchScripts_t.R
+++
@@ -1,8 +0,0 @@
-rm(list=ls()); gc()
-
-# create 200 batch scripts
-for(i in 1: 200) 
-  { tx  <- readLines(paste0("~/Box Sync/JMPH/ClusterFiles_T/Main_T.R"))
-    tx[4] <-paste0("batchNumber <- ",i)
-    writeLines(tx, paste0("~/Box Sync/JMPH/ClusterFiles_T/Main_T",i,".R"))
-}
